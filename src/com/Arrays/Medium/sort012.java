1ee4f2242f0694bb1ba985a8db96ee4e50c8586b
SaiSanjana-Rayaprolu_cdk <saisanjana.rayaprolu@cdk.com>
2023-04-12 23:11:27 +0530

    adding arrays easy and medium

--- src/com/Arrays/Medium/sort012.java
+++ src/com/Arrays/Medium/sort012.java
@@ -45,7 +45,40 @@ public class sort012 {
         }
         return arr;
     }
-    public static void main(String[] args) {
+//    //best approach
+//    //Dutch National Flag Algorithm
+//    //To=(N)
+//    //So(1)
+//
+//    public static void sort012(int arr[]) {
+//        int nz = 0;
+//        int nt = arr.length - 1;
+//        int i=0;
+//
+//        while(i<=nt){
+//            if (arr[i] == 0) {
+//                int temp = arr[i];
+//                arr[i] = arr[nz];
+//                arr[nz] = temp;
+//                nz++;
+//                i++;
+//                //will move i and nz both as we know after swapping it have to be 1 at i
+//
+//            }
+//            else if (arr[i] == 2) {
+//                int temp = arr[i];
+//                arr[i] = arr[nt];
+//                arr[nt] = temp;
+//                nt--;
+//                //remember we will not move i, as we haven't process 2nd half of the array
+//            }
+//            else{
+//                //when arr[i] is 1 , just move i++
+//                i++;
+//            }
+//        }
+
+        public static void main(String[] args) {
         int a[]={2,0,1,0,2};
         sort_num(a);
         for(int i:a)
